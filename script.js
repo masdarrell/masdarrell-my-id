5c29c9c0130454dac2159b1a0c959ff8f3afe20f
titusprasetyo <titusprasetyo@gmail.com>
2025-05-17 18:41:09 +0700

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+//Literal
+
+let NamaFront = "\nNama: ADI";
+let NamaBack = "Darrell";
+let Umur = "\numur: 150";
+let Alamat = "\nAlamat: Jln Del Piero";
+let Namalengkap = NamaFront + " " + NamaBack + " " + Umur + " " + Alamat;
+console.log(Namalengkap);  
